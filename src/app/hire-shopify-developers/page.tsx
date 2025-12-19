8e93b6d332ee7ffdce462de71581c89e2af66813
techbase2brand <techbase2brand@gmail.com>
2025-12-19 16:12:52 +0530

    remove image in hire-shopify-developer while shareing link

--- src/app/hire-shopify-developers/page.tsx
+++ src/app/hire-shopify-developers/page.tsx
@@ -15,18 +15,11 @@ export const metadata = {
       "Hire expert Shopify developers to build fast, scalable, and high-converting Shopify stores.",
     url: "https://www.base2brand.com/hire-shopify-developers",
     siteName: "Base2Brand",
-    images: [
-      {
-        url: "https://www.base2brand.com/img/portfolio/b1.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
     type: "website",
   },
 
   twitter: {
-    card: "summary_large_image",
+    card: "summary",
   },
 };
 
