ee27d1e3bfdf210f4189ba02bcd9ad1511913460
techbase2brand <techbase2brand@gmail.com>
2026-06-11 15:52:38 +0530

    update google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -246,7 +246,7 @@ export default function RootLayout({
 
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WF8GV7S"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KTV3MTCL"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -327,24 +327,24 @@ export default function RootLayout({
           }}
         />
 
-        {/* Google Tag Manager */}
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+{/* Google Tag Manager */}
+<Script
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
       (function(w,d,s,l,i){
         w[l]=w[l]||[];
         w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
         var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),
-            dl=l!='dataLayer'?'&l='+l:'';
+            dl=l!='dataLayer' ? '&l='+l : '';
         j.async=true;
         j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
         f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WF8GV7S');
+      })(window,document,'script','dataLayer','GTM-KTV3MTCL');
     `
-          }}
-        />
+  }}
+/>
         <Script
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
